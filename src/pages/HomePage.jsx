700c5b67474d013f955f8f5929764ba85bf4dc11
Shin <shanu.r9971@gmail.com>
2026-01-20 21:51:22 +0530

    added comparision

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import Hero from "../components/Hero";
 import Features from "../components/Features";
 import HomeGallery from "../components/HomeGallery";
 import FAQ from "../components/Faq";
+import ComparisonTable from "../components/Comparision";
 
 const HomePage = () => (
   <>
     <Hero />
     <Features />
     <HomeGallery />
+    <ComparisonTable />
     <FAQ />
   </>
 );
