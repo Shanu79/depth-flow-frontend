0d7c27fe9c661d516a3de2ccd60250960f219721
Shin <shanu.r9971@gmail.com>
2026-03-31 18:28:13 +0530

    updated api payments

--- src/components/depthflow-api/ApiPricing.jsx
+++ src/components/depthflow-api/ApiPricing.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import useAuthStore from '../stores/authStore.js';
+import useAuthStore from '../../stores/authStore.js';
 
 const ApiPricing = () => {
   const navigate = useNavigate();
